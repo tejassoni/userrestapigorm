f90bde4702a3f25bda837e995a47ce49724dcad4
Tejas Soni <tejas.tejas.soni3@gmail.com>
2026-07-26 11:45:29 +0530

    updated migration logs

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,1 +1,18 @@
-package migrate
+package main
+
+import (
+	"log"
+
+	"userrestapigorm/internal/config"
+	"userrestapigorm/internal/models"
+)
+
+func main() {
+	config.ConnectDB()
+
+	if err := config.GormDB.AutoMigrate(&models.User{}); err != nil {
+		log.Fatal("migration failed: ", err)
+	}
+
+	log.Println("migration completed")
+}
